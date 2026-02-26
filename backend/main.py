743f608e81f1e33dec1671d21d6546cd91b00c17
kinggertingting <phamquockhanh0812@gmail.com>
2026-02-26 20:25:06 +0700

    LLM set up

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+from services.llm import LLMService
+
+# model = 'qwen2:7b'
+# llm = LLMService(model)
+# res = llm.generate_response("Hello")
+# print(res)
+
+
---
+++ backend/services/ingestion.py
@@ -0,0 +1,18 @@
+
+class IngestionService:
+    def __init__(self):
+        self.chunk = 500
+        self.overlap = 50
+        
+        pass
+
+    #Recusrsive chunking
+    def chunking(self, data):
+        pass
+
+    def cleaning(self, data):
+        pass
+
+    def embedding(self, data):
+        pass
+
